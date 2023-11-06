433835337d19e192096bd81e47f97a09f3acc145
Steven Rummler <steven.rummler@gmail.com>
2023-11-05 18:19:13 -0700

    handle main page charts pull correctly

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
+import { kv } from "@vercel/kv";
 import { ChartOptionsForm } from "./ChartOptionsForm";
 
 export default async function Home() {
+  let chartCount = -1;
+
   try {
-    const url = `${process.env.VERCEL_URL}/api/charts`;
-    const result = await fetch(url);
-    const parsed = await result.json();
-    console.log(parsed);
+    const chartKeys = await kv.smembers('charts');
+    // if (chartKeys.length === 0) throw new Error('No charts found');
+    // const chartsTransaction = kv.multi();
+    // chartKeys.forEach(key => chartsTransaction.hgetall(`chart:${key}`));
+    // const charts = await chartsTransaction.exec();
+    chartCount = chartKeys.length;
   } catch (e) {
     console.log(e);
   }
 
   return <main>
     <h1>Embedded Chart Builder</h1>
+    <p>{chartCount} chart{chartCount === 1 ? '' : 's'} built!</p>
     <ChartOptionsForm />
   </main>;
 }
