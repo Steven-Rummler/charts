11a67984f327354b0f81cc4fa51b92a3364483d3
Steven Rummler <steven.rummler@gmail.com>
2023-11-05 17:53:02 -0700

    don't call api from server functions just hit the database directly

--- app/chart/[chart]/page.tsx
+++ app/chart/[chart]/page.tsx
@@ -1,21 +1,17 @@
+import { kv } from "@vercel/kv";
+
 export default async function Chart({ params }: { params: { chart: string } }) {
   const { chart } = params;
-  const url = `${process.env.VERCEL_URL}/api/charts/${chart}`;
-
   try {
-    const result = await fetch(url);
-    const parsed = await result.json();
-    if (parsed === null) return chartNotFound;
+    const chartResult = await kv.hgetall(`chart:${chart}`);
     return <main>
       <h1>Embedded Chart</h1>
-      <p>{JSON.stringify(parsed)}</p>
+      <p>{JSON.stringify(chartResult)}</p>
     </main>;
   } catch (e) {
     console.log(e);
-    return chartNotFound;
+    return <main>
+      <h1>Chart Not Found</h1>
+    </main>;
   }
 }
-
-const chartNotFound = <main>
-  <h1>Chart Not Found</h1>
-</main>;
